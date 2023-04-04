1dc44236c1fa7263eb501a57a210ce4e82d80656
junlin <1437125144@qq.com>
2023-04-04 21:17:36 +0800

    iTop4412

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Apr 2 22:32:09 CST 2023"
+#define LINUX_COMPILE_BY "junlin"
+#define LINUX_COMPILE_HOST "junlin"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
